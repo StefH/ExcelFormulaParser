6f0a4827428fac934e06677bc33b168da5484aa3
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-05-27 22:20:38 +0200

    Add unit-test : ParseAddNumbers

--- tests/ExcelFormulaParser.Tests/ExcelFormulaTests.cs
+++ tests/ExcelFormulaParser.Tests/ExcelFormulaTests.cs
@@ -6,7 +6,7 @@ namespace ExcelFormulaParser.Tests
     public class ExcelFormulaTests
     {
         // ReSharper disable once InconsistentNaming
-        private ExcelFormula excelFormula;
+        private ExcelFormula ef;
 
         [Fact]
         public void ExcelFormula_ParseNumber()
@@ -15,13 +15,37 @@ namespace ExcelFormulaParser.Tests
             const string formula = "=42";
 
             // Act
-            excelFormula = new ExcelFormula(formula);
+            ef = new ExcelFormula(formula);
 
             // Assert
-            Check.That(excelFormula.Count).Equals(1);
-            Check.That(excelFormula[0].Value).Equals("42");
-            Check.That(excelFormula[0].Type).Equals(ExcelFormulaTokenType.Operand);
-            Check.That(excelFormula[0].Subtype).Equals(ExcelFormulaTokenSubtype.Number);
+            Check.That(ef.Count).Equals(1);
+            Check.That(ef[0].Value).Equals("42");
+            Check.That(ef[0].Type).Equals(ExcelFormulaTokenType.Operand);
+            Check.That(ef[0].Subtype).Equals(ExcelFormulaTokenSubtype.Number);
+        }
+
+        [Fact]
+        public void ExcelFormula_ParseAddNumbers()
+        {
+            // Assign
+            const string formula = "=1+2";
+
+            // Act
+            ef = new ExcelFormula(formula);
+
+            // Assert
+            Check.That(ef.Count).Equals(3);
+            Check.That(ef[0].Value).Equals("1");
+            Check.That(ef[0].Type).Equals(ExcelFormulaTokenType.Operand);
+            Check.That(ef[0].Subtype).Equals(ExcelFormulaTokenSubtype.Number);
+
+            Check.That(ef[1].Value).Equals("+");
+            Check.That(ef[1].Type).Equals(ExcelFormulaTokenType.OperatorInfix);
+            Check.That(ef[1].Subtype).Equals(ExcelFormulaTokenSubtype.Math);
+
+            Check.That(ef[2].Value).Equals("2");
+            Check.That(ef[2].Type).Equals(ExcelFormulaTokenType.Operand);
+            Check.That(ef[2].Subtype).Equals(ExcelFormulaTokenSubtype.Number);
         }
     }
 }
