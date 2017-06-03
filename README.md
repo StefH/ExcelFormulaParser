a6d9629518e3a49e119122e82934ed082886d53f
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-06-03 22:05:18 +0200

    tests and codecov

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ An Excel Formula Parser, based on http://ewbi.blogs.com/develops/2007/03/excel_f
 
 [![Build status](https://ci.appveyor.com/api/projects/status/3rh96qm7840gvjuf?svg=true)](https://ci.appveyor.com/project/StefH/excelformulaparser)
 [![NuGet Badge](https://buildstats.info/nuget/ExcelFormulaParser)](https://www.nuget.org/packages/ExcelFormulaParser)
+[![codecov](https://codecov.io/gh/StefH/ExcelFormulaParser/branch/master/graph/badge.svg)](https://codecov.io/gh/StefH/ExcelFormulaParser)
+
 
 Supported frameworks:
 - net 2.0
---
+++ tests/ExcelFormulaExpressionParser.Tests/OperandFunctionTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq.Expressions;
+using ExcelFormulaParser;
+using NFluent;
+using Xunit;
+
+namespace ExcelFormulaExpressionParser.Tests
+{
+    public class OperandFunctionTests
+    {
+        [Fact]
+        public void OperandFunction_And()
+        {
+            // Assign
+            var formula = new ExcelFormula("=AND(TRUE, TRUE, FALSE)");
+
+            // Act
+            Expression expression = new ExpressionParser(formula).Parse();
+            var result = Expression.Lambda(expression).Compile().DynamicInvoke();
+
+            // Assert
+            Check.That(result).IsEqualTo(false);
+        }
+
+        [Fact]
+        public void OperandFunction_If_False()
+        {
+            // Assign
+            var formula = new ExcelFormula("=IF(FALSE, 42, -1)");
+
+            // Act
+            Expression expression = new ExpressionParser(formula).Parse();
+            var result = Expression.Lambda(expression).Compile().DynamicInvoke();
+
+            // Assert
+            Check.That(result).IsEqualTo(-1);
+        }
+
+        [Fact]
+        public void OperandFunction_If_True()
+        {
+            // Assign
+            var formula = new ExcelFormula("=IF(TRUE, 42, -1)");
+
+            // Act
+            Expression expression = new ExpressionParser(formula).Parse();
+            var result = Expression.Lambda(expression).Compile().DynamicInvoke();
+
+            // Assert
+            Check.That(result).IsEqualTo(42);
+        }
+
+        [Fact]
+        public void OperandFunction_Or()
+        {
+            // Assign
+            var formula = new ExcelFormula("=OR(TRUE, TRUE, FALSE)");
+
+            // Act
+            Expression expression = new ExpressionParser(formula).Parse();
+            var result = Expression.Lambda(expression).Compile().DynamicInvoke();
+
+            // Assert
+            Check.That(result).IsEqualTo(true);
+        }
+    }
+}
